325f66243a01f30425ec68ba7648802941d14162
Armand Packham-McGuiness <armand.packham@gmail.com>
2024-06-29 17:02:31 +1000

    idk

--- main.py
+++ main.py
@@ -10,9 +10,20 @@ player = pygame.Rect((300,250,50,50))
 
 while True:
 
+  screen.fill((0,0,0))
+  
   pygame.draw.rect(screen, (255,0,0), player)
 
-  
+  key = pygame.key.get_pressed()
+  if key[pygame.K_a] == True:
+      player.move_ip(-1,0)
+  if key[pygame.K_d] == True:
+      player.move_ip(1,0)
+  if key[pygame.K_w] == True:
+      player.move_ip(0,-1)
+  if key[pygame.K_s] == True:
+      player.move_ip(0,1)
+      
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
